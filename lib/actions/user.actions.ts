88c4dcc5ac2770061920b5ec698a8d5f23dc0669
yondraseptian <yondraseptian@gmail.com>
2024-09-19 22:32:46 +0700

    create ui auth for user

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,16 @@
+'user server'
+
+export const signIn = async () => {
+    try {
+        //Mutation / Database / Make fetch
+    } catch (error) {
+        console.log('Error',error)        
+    }
+}
+export const signUp = async (userData : SignUpParams) => {
+    try {
+        //Create a user account
+    } catch (error) {
+        console.log('Error',error)        
+    }
+}
